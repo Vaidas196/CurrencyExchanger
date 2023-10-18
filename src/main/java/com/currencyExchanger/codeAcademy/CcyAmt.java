0ed4113ece0a21657e6af86b582b5f23cd17831b
vaidas <vitkunas.vaidas@gmail.com>
2023-10-18 20:46:25 +0300

    [git] Project cleaned from Jackson annotations

--- src/main/java/com/currencyExchanger/codeAcademy/CcyAmt.java
+++ src/main/java/com/currencyExchanger/codeAcademy/CcyAmt.java
@@ -1,55 +1,11 @@
 package com.currencyExchanger.codeAcademy;
-import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
-import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
 import lombok.*;
 @Data
-
-
-@JacksonXmlRootElement(localName = "CcyAmt")
+@AllArgsConstructor
+@NoArgsConstructor
 class CcyAmt {
 
-
-    @JacksonXmlProperty(localName = "Ccy")
     private String ccy;
-
-    @JacksonXmlProperty(localName = "Amt")
     private double amt;
 
-
-
-    public CcyAmt() {
-    }
-
-    public CcyAmt(String ccy) {
-        this.ccy = ccy;
-    }
-
-    public CcyAmt(double amt) {
-        this.amt = amt;
-    }
-
-    public CcyAmt(String ccy, double amt) {
-        this.ccy = ccy;
-        this.amt = amt;
-    }
-
-    // Getters and setters
-
-    @JacksonXmlProperty(localName = "Ccy")
-    public String getCcy() {
-        return ccy;
-    }
-    @JacksonXmlProperty(localName = "Ccy")
-    public void setCcy(String ccy) {
-        this.ccy = ccy;
-    }
-
-    @JacksonXmlProperty(localName = "Amt")
-    public double getAmt() {
-        return amt;
-    }
-    @JacksonXmlProperty(localName = "Amt")
-    public void setAmt(double amt) {
-        this.amt = amt;
-    }
 }

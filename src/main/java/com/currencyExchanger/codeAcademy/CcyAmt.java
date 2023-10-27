a83b97eebe9e5657b17bf00db8b4a14aaab70c5e
vaidas <vitkunas.vaidas@gmail.com>
2023-10-27 19:15:34 +0300

    [git] Project Reads Objects from xmlFile

--- src/main/java/com/currencyExchanger/codeAcademy/CcyAmt.java
+++ src/main/java/com/currencyExchanger/codeAcademy/CcyAmt.java
@@ -1,11 +1,16 @@
 package com.currencyExchanger.codeAcademy;
+import com.thoughtworks.xstream.annotations.XStreamAlias;
 import lombok.*;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@XStreamAlias("CcyAmt")
 class CcyAmt {
 
+    @XStreamAlias("Ccy")
     private String ccy;
+
+    @XStreamAlias("Amt")
     private double amt;
 
 }
--- src/main/java/com/currencyExchanger/codeAcademy/FxRates.java
+++ src/main/java/com/currencyExchanger/codeAcademy/FxRates.java
@@ -1,10 +1,17 @@
 package com.currencyExchanger.codeAcademy;
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+import com.thoughtworks.xstream.annotations.XStreamImplicit;
+import com.thoughtworks.xstream.annotations.XStreamInclude;
 import lombok.*;
 import java.util.*;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@XStreamAlias("FxRates")
 public class FxRates {
+
+    @XStreamImplicit
     private List<FxRate> fxRateList = new ArrayList<>();
+
 }
